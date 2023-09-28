5f74460f2dfc3852811ae2029892a91bdabac9a7
John Nordwall <johnfromchicago@gmail.com>
2023-09-28 13:49:08 -0500

    fixed commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            string mySonsName = "Clark";
+            int hisAge = 13;
+            char hisMiddeInitial = 'A';
+            bool likesStarTrek = false;
+            double hisEarnedRunAverage = 3.01;
+            decimal carriesWithHim = 10.50m;
+            Console.WriteLine($"My son's name is {mySonsName}, and he is {hisAge}. He loves pitching.");
+            Console.WriteLine($"His ERA is {hisEarnedRunAverage}, and he always has ${carriesWithHim} on him.");
+        }
+    }
+}
